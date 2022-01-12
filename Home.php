0e3801f324c39ec7a11e70f1cdee8ec939529a82
Ravindu Dhanushka <guestpc87@gmail.com>
2022-01-12 21:27:23 +0530

    Added WEB Application Project

---
+++ Home.php
@@ -0,0 +1,94 @@
+<!Doctype html>
+<html>
+<head>
+    <style><?php include 'Home.css' ?></style>
+    <title>Home Page</title>
+</head>
+<body>
+    <?php echo("<img src='Images/nsbm_logo_favicon.png' alt='NSBM LOGO' class='round'>") ?>
+
+    <?php echo("<img src='Images/Profile.png' alt='NSBM LOGO' class='round1' style='position: absolute; top: 100px; left: 1350px'>") ?>
+
+    <table style="position: absolute; top: 100px; left: 300px">
+        <tr>
+            <td><a href="Home.php"><input type='button' value='Home' class="button"></td>
+            <td><input type='button' value='News' class="button"></td>
+            <td><input type='button' value='About Us' class="button"></td>
+            <td><input type='button' value='Contact Us' class="button"></td>
+        </tr>
+    </table>
+    <table style="position: absolute; top: 100px; left: 1000px">
+        <tr>
+            <td><input type="button" value="Sign In" class="button1"></td>
+            <td><input type="button" value="Sign Up" class="button1"></td>
+        </tr>
+    </table>
+<p class="font">NSBM <font color="#02CD22">SPORTS</font></p>
+
+<div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+</div>
+
+<table width="1500px">
+    <tr>
+        <td><img src="Images/Sports/Ellipse%201.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%202.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%203.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%204.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%2010.png" class="sports"></td>
+    </tr>
+    <tr>
+        <td></td>
+    </tr>
+    <tr>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Badminton' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Basketball' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Cricket' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Athletics' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Rugby' class="button"></td>
+    </tr>
+</table>
+<br>
+<br>
+<table width="1250px">
+    <tr>
+        <td><img src="Images/Sports/Ellipse%206.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%207.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%208.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%209.png" class="sports"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+    </tr>
+    <tr>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Swimming' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Table Tennis' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Net Ball' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Martial Arts' class="button"></td>
+    </tr>
+</table>
+<div style="position: absolute; left: 1275px; top: 925px">
+    <a href="Sports.php"><input type="button" value="See More" style="background-color: #3AB54A; color: white; border-color: #3AB54A; border-radius: 80px; font-size: 24px; padding: 10px 30px; font-family: 'roboto-slab';"></a>
+</div>
+<br>
+<footer class="footer">
+    <?php
+        echo ("<center>© 2021 NSBM Sports. Designed By XXXXXXX 21.1</center>");
+    ?>
+</footer>
+</body>
+</html>
---
+++ Sports.php
@@ -0,0 +1,112 @@
+<!Doctype html>
+<html>
+<head>
+    <style>
+        <?php include 'Home.css' ?>
+    </style>
+    <title>Sports</title>
+</head>
+<body>
+<?php echo("<img src='Images/nsbm_logo_favicon.png' alt='NSBM LOGO' class='round'>") ?>
+
+<?php echo("<img src='Images/Profile.png' alt='NSBM LOGO' class='round1' style='position: absolute; top: 100px; left: 1350px'>") ?>
+
+<table style="position: absolute; top: 100px; left: 300px">
+    <tr>
+        <td><a href="Home.php"><input type='button' value='Home' class="button"></td>
+        <td><input type='button' value='News' class="button"></td>
+        <td><input type='button' value='About Us' class="button"></td>
+        <td><input type='button' value='Contact Us' class="button"></td>
+    </tr>
+</table>
+<table style="position: absolute; top: 100px; left: 1000px">
+    <tr>
+        <td><input type="button" value="Sign In" class="button1"></td>
+        <td><input type="button" value="Sign Up" class="button1"></td>
+    </tr>
+</table>
+
+<?php echo ("<br>") ?>
+<?php echo ("<br>") ?>
+<?php echo ("<br>") ?>
+
+<?php echo ("<center style='font-family: rozha; font-size: 72px;'>Sports Clubs</center>") ?>
+
+<?php echo ("<br>") ?>
+<?php echo ("<br>") ?>
+
+<table width="1500px">
+    <tr>
+        <td><img src="Images/Sports/Ellipse%201.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%202.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%203.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%204.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%2010.png" class="sports"></td>
+    </tr>
+    <tr>
+        <td></td>
+    </tr>
+    <tr>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Badminton' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Basketball' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Cricket' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Athletics' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Rugby' class="button"></td>
+    </tr>
+</table>
+
+<?php echo ("<br>") ?>
+<?php echo ("<br>") ?>
+
+<table width="1500px">
+    <tr>
+        <td><img src="Images/Sports/Ellipse%201.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%202.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%203.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%204.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%2010.png" class="sports"></td>
+    </tr>
+    <tr>
+        <td></td>
+    </tr>
+    <tr>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Badminton' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Basketball' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Cricket' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Athletics' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Rugby' class="button"></td>
+    </tr>
+</table>
+
+<?php echo ("<br>") ?>
+<?php echo ("<br>") ?>
+
+<table width="1500px">
+    <tr>
+        <td><img src="Images/Sports/Ellipse%201.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%202.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%203.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%204.png" class="sports"></td>
+        <td><img src="Images/Sports/Ellipse%2010.png" class="sports"></td>
+    </tr>
+    <tr>
+        <td></td>
+    </tr>
+    <tr>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Badminton' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Basketball' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Cricket' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Athletics' class="button"></td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='button' value='Rugby' class="button"></td>
+    </tr>
+</table>
+
+<br>
+<footer class="footer">
+    <?php
+    echo ("<center>© 2021 NSBM Sports. Designed By XXXXXXX 21.1</center>");
+    ?>
+</footer>
+
+</body>
+</html>
