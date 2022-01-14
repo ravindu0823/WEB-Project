8e645966066a0ba8c1f9827ac354940f627cef61
Ravindu Dhanushka <guestpc87@gmail.com>
2022-01-14 15:47:09 +0530

    Create SignIn.css an SignUp.css

---
+++ SignIn.php
@@ -0,0 +1,43 @@
+<!Doctype html>
+<html>
+<head>
+    <style><?php include 'SignIn.css' ?></style>
+    <title>Sign In</title>
+</head>
+<body>
+<table>
+    <tr>
+        <td><img src='Images/nsbm_logo_favicon.png' alt='NSBM LOGO' class='round'></td>
+        <td><a href="Home.php"><input type='button' value='Home' class="button"></td>
+        <td><input type='button' value='News' class="button"></td>
+        <td><input type='button' value='About Us' class="button"></td>
+        <td><input type='button' value='Contact Us' class="button"></td>
+        <td width="300px"></td>
+        <td><input type="button" value="Sign In" class="button1"></td>
+        <td><input type="button" value="Sign Up" class="button1"></td>
+        <td><img src='Images/Profile.png' alt='NSBM LOGO' class="round1"></td>
+    </tr>
+</table>
+<br><br><br><br><br><br><br>
+<table border="1">
+    <tr>
+       <td rowspan="6" width="1000px"><center><img src="Images/Group%201.png" class="logo"></center></td>
+    </tr>
+    <tr>
+        <td>Sign In</td>
+    </tr>
+    <tr>
+        <td>Index No</td>
+    </tr>
+    <tr>
+        <td><input type="text" name="indexno" placeholder="Index No"></td>
+    </tr>
+    <tr>
+        <td>Password</td>
+    </tr>
+    <tr>
+        <td><input type="password" name="pass" placeholder="Password"></td>
+    </tr>
+</table>
+</body>
+</html>
