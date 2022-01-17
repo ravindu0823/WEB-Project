38337743b423e4b7f85eb98e55387093c4983f83
Ravindu Dhanushka <guestpc87@gmail.com>
2022-01-17 07:53:38 +0530

    Add new php validation

--- NEW/application/views/register.php
+++ NEW/application/views/register.php
@@ -2,6 +2,81 @@
 <?php include './layout/navbar.php' ?>
 
 <?php
+/**
+ * Controller
+ */
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errors = [];
+
+    // PHP form validation
+    $student_id = trim($_POST['student_id']);
+    $name = trim($_POST['name']);
+    $age = trim($_POST['age']);
+    $faculty = trim($_POST['faculty']);
+    $email = trim($_POST['email']);
+    $phone = trim($_POST['phone']);
+    $address = trim($_POST['address']);
+
+    if (!$student_id) {
+        array_push($errors, 'Enter your Student ID');
+    }
+
+    if (!filter_var($student_id, FILTER_VALIDATE_INT)) {
+        array_push($errors, "$student_id is not a valid Student ID");
+    }
+
+    if ($student_id < 5) {
+        array_push($errors, 'Student ID must have 5 digits');
+    }
+
+    if (!$name) {
+        array_push($errors, 'Enter your Name');
+    }
+
+    if (!preg_match('/^[a-zA-Z ]+$/i', $name)) {
+        array_push($errors, 'Provide a valid Name');
+    }
+
+    if (!$age) {
+        array_push($errors, 'Please enter your age');
+    }
+
+    if (filter_var($age, FILTER_VALIDATE_INT)) {
+        array_push($errors, 'Age should be a numeric value');
+    }
+
+    if ($age > 2) {
+        array_push($errors, 'Enter a valid age');
+    }
+
+    if (!$faculty) {
+        array_push($errors, 'Please select your faculty');
+    }
+
+    if (!$email) {
+        array_push($errors, 'Enter your student Email address');
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($errors, "$email is not a valid Email address");
+    }
+
+    // Validate form
+    if (sizeof($errors) > 0) {
+        // Validation unsuccessful
+        $_SESSION['form_validation']['errors'] = $errors;
+        header('Location: register.php');
+    } else {
+        // Validation successful
+        unset($_SESSION['form_validation']['errors']);
+
+        // TODO: DB connection
+    }
+}
+?>
+
+<?php
+// Form meta data
 $faculties = [
     [
         'id' => '1',
@@ -22,8 +97,8 @@ $faculties = [
 ];
 ?>
 
-    <div class="container-fluid pt-5 pb-5"
-         style="background: url('../../public/images/Rectangle 9.png') no-repeat; background-size: cover;">
+    <div class="container-fluid pt-5"
+         style="background: url('../../public/images/Rectangle 34.png') no-repeat; background-size: cover; padding-bottom: 210px">
         <div class="row">
             <div class="col-lg-12">
                 <div class="container">
@@ -33,29 +108,35 @@ $faculties = [
                         </div>
                         <div class="col-lg-6 reg-form">
 
-                            <form name="register" id="register" method="POST" action="register.php">
+                            <?php if ($_SESSION['form_validation']['errors']) { ?>
+                                <div class="alert bg-danger">
+                                    <?php foreach ($_SESSION['form_validation']['errors'] as $error) { ?>
+                                        <p><?php echo $error; ?></p>
+                                    <?php } ?>
+                                </div>
+                            <?php } ?>
+
+                            <form name="register" id="register" method="POST" action="register.php"
+                                  onsubmit="return formValidation();">
 
                                 <div class="input-group mb-3">
                                     <span class="input-group-text" id="basic-addon1">Student ID</span>
                                     <input type="text" name="student_id" id="student_id" class="form-control"
-                                           placeholder="NSBM Student ID Number"
-                                           aria-label="Username"
-                                           aria-describedby="basic-addon1" min="5" minlength="5" max="5" maxlength="5"
-                                           required autofocus>
+                                           placeholder="NSBM Student ID Number" aria-label="Username" min="5"
+                                           minlength="5" max="5" maxlength="5" required autofocus>
                                 </div>
 
                                 <div class="input-group mb-3">
                                     <span class="input-group-text" id="basic-addon1">Name</span>
-                                    <input type="text" class="form-control" placeholder="Full Name"
-                                           aria-label="Name" name="name" id="name"
-                                           aria-describedby="basic-addon1" max="100" min="5" maxlength="100" required>
+                                    <input type="text" name="name" id="name" class="form-control"
+                                           placeholder="Full Name" aria-label="Name" min="5" max="100" maxlength="100"
+                                           required>
                                 </div>
 
                                 <div class="input-group mb-3">
                                     <span class="input-group-text" id="basic-addon1">Age</span>
-                                    <input type="text" class="form-control" placeholder="Age" aria-label="Age"
-                                           name="age" id="age"
-                                           aria-describedby="basic-addon1" min="1" max="2" maxlength="2" required>
+                                    <input type="text" name="age" id="age" class="form-control" placeholder="Age"
+                                           aria-label="Age" min="1" max="2" maxlength="2" required>
                                 </div>
 
                                 <div class="input-group mb-3">
@@ -69,32 +150,27 @@ $faculties = [
                                 </div>
 
                                 <div class="input-group mb-3">
-                                    <input type="text" class="form-control" placeholder="Student Email Address"
-                                           aria-label="Recipient's username" name="email" id="email"
-                                           aria-describedby="basic-addon2" min="5"
+                                    <span class="input-group-text" id="basic-addon1">Email</span>
+                                    <input type="text" name="email" id="email" class="form-control"
+                                           placeholder="Student Email Address" aria-label="Recipient's username" min="5"
                                            max="80" required>
-                                    <span class="input-group-text" id="basic-addon2">@students.nsbm.ac.lk</span>
                                 </div>
 
                                 <div class="input-group mb-3">
                                     <span class="input-group-text" id="basic-addon1">Phone Number</span>
-                                    <input type="text" name="phone_number" id="phone_number" class="form-control"
-                                           placeholder="Phone Number"
-                                           aria-label="Username"
-                                           aria-describedby="basic-addon1" min="10" max="10" maxlength="10">
+                                    <input type="text" name="phone" id="phone" class="form-control"
+                                           placeholder="Phone Number" aria-label="Username" min="10" max="10"
+                                           maxlength="10">
                                 </div>
 
                                 <div class="input-group mb-3">
                                     <span class="input-group-text" id="basic-addon1">Address</span>
                                     <input type="text" name="address" id="address" class="form-control"
-                                           placeholder="Home Address"
-                                           aria-label="Address"
-                                           aria-describedby="basic-addon1" required min="10" max="100" maxlength="100">
+                                           placeholder="Home Address" aria-label="Address" required min="10" max="100"
+                                           maxlength="100">
                                 </div>
 
-                                <button type="submit" class="btn btn-primary d-block w-100"
-                                        onclick="ValidateRegister()">Register
-                                </button>
+                                <button type="submit" class="btn btn-primary d-block w-100">Register</button>
 
                             </form>
 
@@ -107,53 +183,52 @@ $faculties = [
     </div>
 
     <script type="text/javascript">
-        let errors = [];
+        function formValidation() {
+            let errors = [];
+            let errors_string = '';
 
-        function ValidateRegister() {
             if (!document.getElementById('student_id').value) {
-                errors.push("Enter the Student ID");
+                errors.push('Enter your Student ID');
             }
 
             if (document.getElementById('student_id').value < 5) {
-                errors.push("Student ID must have 5 digits");
+                errors.push('Student ID must have 5 digits');
             }
 
             if (!document.getElementById('name').value) {
-                errors.push("Enter your name");
+                errors.push('Enter your Name');
             }
 
             if (!document.getElementById('age').value) {
-                errors.push("Enter your age");
+                errors.push('Enter your Age');
             }
 
             if (isNaN(document.getElementById('age').value)) {
-                errors.push("Age must be a numeric value");
+                errors.push('Age must be a numeric value');
             }
 
             if (!document.getElementById('faculty').value) {
-                errors.push("Please select your faculty");
+                errors.push('Select your Faculty');
             }
 
             if (!document.getElementById('email').value) {
-                errors.push("Enter your student email address");
+                errors.push('Enter your student Email address');
             }
 
             let a = document.getElementById('email').value.indexOf('@');
             let b = document.getElementById('email').value.lastIndexOf('.');
 
             if (a < 1 || b - a < 2 || b + 2 >= document.getElementById('email').value.length) {
-                errors.push("Provide a Valid E-mail Address");
+                errors.push('Provide a valid Email Address');
             }
 
-            if (document.getElementById('phone_number').value.length < 10) {
-                errors.push("Enter a valid Phone number");
+            if (document.getElementById('phone').value.length < 10) {
+                errors.push('Enter your Phone number');
             }
 
             if (!document.getElementById('address').value) {
-                errors.push("Enter your address");
+                errors.push('Enter your Address');
             }
-
-            let errors_string = '';
 
             // Loop through all errors
             for (let i = 0; i < errors.length; i++) {
@@ -165,7 +240,13 @@ $faculties = [
                 }
             }
 
-            window.alert(errors_string);
+            // Stop form submission if there're errors
+            if (errors.length > 0) {
+                window.alert(errors_string);
+                return false;
+            }
+
+            return true;
         }
     </script>
 
