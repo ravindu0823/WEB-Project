89a064e76bb5397de4947338c0255c4715ae6038
Ravindu Dhanushka <guestpc87@gmail.com>
2022-01-27 13:05:22 +0530

    add a logo to the title

--- NEW/application/views/layout/header.php
+++ NEW/application/views/layout/header.php
@@ -3,6 +3,9 @@
 <head>
     <title><?php echo 'NSBM sports'; ?></title>
 
+    <!-- Add icon link -->
+    <link rel="icon" href="../../public/images/nsbm_logo_favicon.png" type="image/x-icon">
+
     <!-- Bootstrap -->
     <link href="../../public/css/bootstrap.css" rel="stylesheet">
     <script src="../../public/js/bootstrap.js" defer></script>
