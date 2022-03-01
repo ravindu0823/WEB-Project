ef3df166d066152717bf537ae7595ba2914f2023
Tharindu Umayanga <tharinduumayangat@gmail.com>
2022-03-01 15:55:02 +0530

    NewUpdate

---
+++ web.js
@@ -0,0 +1,9 @@
+document.getElementById('button2').addEventListener('click',
+function(){
+    document.querySelector('.bg-modal').style.display='flex'
+});
+
+document.querySelector('.close').addEventListener('click',
+function(){
+    document.querySelector('.bg-modal').style.display='none'
+});
