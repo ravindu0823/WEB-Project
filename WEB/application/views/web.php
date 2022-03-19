70c92346c2bb813b58eb1991f2a2a0fb7b5e8367
Ravindu Dhanushka <rrdperera@students.nsbm.ac.lk>
2022-03-19 20:20:33 +0530

    Create web.php

---
+++ WEB/application/views/web.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="../../public/css/web.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title><?php echo 'NSBM sports'; ?></title>
+
+    <!-- Add icon link -->
+    <link rel="icon" href="../../public/images/nsbm_logo_favicon.png" type="image/x-icon">
+</head>
+
+<?php include './layout/navbar.php' ?>
+
+<body class="ColorClass2" style="width: 100%;">
+<div class="container-fluid" style="padding: 0;">
+<div>
+    <img src="../../public/images/nsbmsof.png" alt="nsbm" class="img-fluid">
+    <br><br><br>
+    
+    <h1 class="ColorClass">ABOUT US</h1>
+    <br><br>
+
+
+<p class="MyClass1">Our Software is mainly contrubute in NSBM Sports Counsil 
+    you can go trough our web page into your sport account interface
+</p>
+</div>
+<br><br><hr>
+
+<!-- Our Members In Project -->
+
+<div class="row1">
+  <div class="column">
+    <div class="card1">
+      <img src="../../public/images/pp.jpg" alt="Jane" style="width:100%" class="img-fluid">
+      <div class="container">
+        
+        <p class="title">Designer</p>
+        <p class="ColorClass3">Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p class="ColorClass1">john@example.com</p>
+        <p><a href="ravindu.html"><button class="button">More Details</button></a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card1">
+      <img src="../../public/images/pp.jpg" alt="Mike" style="width:100%" class="img-fluid">
+      <div class="container">
+        
+        <p class="title">Designer</p>
+        <p class="ColorClass3">Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p class="ColorClass1">john@example.com</p>
+        <p><a href="ravindu.html"><button class="button">More Details</button></a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card1">
+      <img src="../../public/images/pp.jpg" alt="John" style="width:100%" class="img-fluid">
+      <div class="container">
+        
+        <p class="title">Designer</p>
+        <p class="ColorClass3">Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p class="ColorClass1">john@example.com</p>
+        <p><a href="ravindu.html"><button class="button">More Details</button></a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card1">
+      <img src="../../public/images/pp.jpg" alt="John" style="width:100%" class="img-fluid">
+      <div class="container">
+        
+        <p class="title">Designer</p>
+        <p class="ColorClass3">Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p class="ColorClass1">john@example.com</p>
+        <p><a href="ravindu.html"><button class="button">More Details</a></button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card1">
+      <img src="../../public/images/pp.jpg" alt="John" style="width:100%" class="img-fluid">
+      <div class="container">
+        
+        <p class="title">Designer</p>
+        <p class="ColorClass3">Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p class="ColorClass1">john@example.com</p>
+        <p><a href="ravindu.html"><button class="button">More Details</button></a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card1">
+      <img src="../../public/images/pp.jpg" alt="John" style="width:100%" class="img-fluid">
+      <div class="container">
+        
+        <p class="title">Designer</p>
+        <p class="ColorClass3">Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p class="ColorClass1">john@example.com</p>
+        <p><a href="ravindu.html"><button class="button">More Details</button></a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <footer class="footer bg-dark" style="position: fixed; bottom: 0; width: 100%; color: white; text-align: center;">
+        <span>© 2021 NSBM Sports. Designed By XXXXXXX 21.1</span>
+    </footer>
+
+</div>
+
+</body>
