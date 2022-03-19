a72d7b20992861b9c333768ed08fa75a7696677b
Ravindu Dhanushka <rrdperera@students.nsbm.ac.lk>
2022-03-19 20:19:01 +0530

    Update logout.php

--- web/application/views/logout.php
+++ web/application/views/logout.php
@@ -1,9 +1,11 @@
 <?php
-
 session_start();
 
-unset($_SESSION['username']);
+unset($_SESSION['logged_in']);
+unset($_SESSION['user_data']);
+
+$success = ['Logout Successfully'];
+$_SESSION['form_validation']['success'] = $success;
+
 header('Location: login.php');
-
-
 ?>
